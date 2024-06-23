4df7510069b858b19e20022e1fc0c6b68829d596
Fdzak01 <fdzak01@gmail.com>
2024-06-23 15:26:48 +0700

    Membuat Migration dari masing-masing Table db

---
+++ database/migrations/2024_06_23_073435_create_pegawais_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->string('nip')->primary();
+            $table->enum('gender', ['Laki-Laki','Perempuan']);
+            $table->string('tmp_lahir');
+            $table->date('tgl_lahir');
+            $table->string('telpon');
+            $table->string('alamat');
+            $table->foreignId('divisi_id')->constrained('divisis')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawais');
+    }
+};
