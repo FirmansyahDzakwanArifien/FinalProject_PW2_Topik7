20daff6a48aa67f1b585c33da71e57e0a26d776f
ilyasbdlzz <ilyaskenway@gmail.com>
2024-07-07 13:55:09 +0700

    File routes tadi ketinggalan

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
+
 use App\Http\Controllers\DivisiController;
-use App\Http\Controllers\JatahCutiController;
 use App\Http\Controllers\PegawaiController;
-use App\Http\Controllers\PengajuanController;
+use App\Http\Controllers\PengajuaCutiController;
+use App\Http\Controllers\JatahCutiController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Pegawai
+Route::get('/dashboard/pegawai', [PegawaiController::class, 'index']);
+Route::get('/dashboard/pegawai/create', [PegawaiController::class, 'create']);
+Route::post('/dashboard/pegawai', [PegawaiController::class, 'store']);
+Route::get('/dashboard/pegawai/{nip}', [PegawaiController::class, 'show'])->name('pegawai.show');
+Route::get('/dashboard/pegawai/edit/{nip}', [PegawaiController::class, 'edit']);
+Route::put('/dashboard/pegawai/update/{nip}', [PegawaiController::class, 'update']);
+Route::delete('/dashboard/pegawai/destroy/{id}', [PegawaiController::class, 'destroy']);
 
-Route::get('dashboard', [AdminController::class, 'index']);
-Route::get('dashboard/divisi', [DivisiController::class, 'index']);
-Route::get('dashboard/pegawai', [PegawaiController::class, 'index']);
-Route::get('dashboard/jatahcuti', [JatahCutiController::class, 'index']);
-Route::get('dashboard/pengajuan', [PengajuanController::class, 'index']);
+
+
+//Divisi
+Route::get('/dashboard/divisi', [DivisiController::class, 'index']);
+Route::get('/dashboard/divisi/create', [DivisiController::class, 'create']);
+Route::post('/dashboard/divisi', [DivisiController::class, 'store']);
+Route::get('/dashboard/divisi/{id}', [DivisiController::class, 'show'])->name('divisi.show');
+Route::get('/dashboard/divisi/edit/{id}', [DivisiController::class, 'edit']);
+Route::put('/dashboard/divisi/update/{id}', [DivisiController::class, 'update']);
+Route::delete('/dashboard/divisi/destroy/{id}', [DivisiController::class, 'destroy']);
+
+//Pengajuan Cuti
+Route::get('/dashboard/pengajuan_cuti', [PengajuaCutiController::class, 'index']);
+Route::get('/dashboard/pengajuan_cuti/create', [PengajuaCutiController::class, 'create']);
+Route::post('/dashboard/pengajuan_cuti', [PengajuaCutiController::class, 'store']);
+Route::get('/dashboard/pengajuan_cuti/{id}', [PengajuaCutiController::class, 'show'])->name('pengajuan_cuti.show');
+Route::get('/dashboard/pengajuan_cuti/edit/{id}', [PengajuaCutiController::class, 'edit']);
+Route::put('/dashboard/pengajuan_cuti/update/{id}', [PengajuaCutiController::class, 'update']);
+Route::delete('/dashboard/pengajuan_cuti/destroy/{id}', [PengajuaCutiController::class, 'destroy']);
+
+//Jatah Cuti
+Route::get('/dashboard/jatah_cuti', [JatahCutiController::class, 'index']);
+Route::get('/dashboard/jatah_cuti/create', [JatahCutiController::class, 'create']);
+Route::post('/dashboard/jatah_cuti', [JatahCutiController::class, 'store']);
+Route::get('/dashboard/jatah_cuti/{id}', [JatahCutiController::class, 'show'])->name('jatah_cuti.show');
+Route::get('/dashboard/jatah_cuti/edit/{id}', [JatahCutiController::class, 'edit']);
+Route::put('/dashboard/jatah_cuti/update/{id}', [JatahCutiController::class, 'update']);
+Route::delete('/dashboard/jatah_cuti/destroy/{id}', [JatahCutiController::class, 'destroy']);
